3bd53d08f26229da39422cbf760fbbe74741af17
Lourousa <22854771+Lourousa@users.noreply.github.com>
2025-01-24 19:00:57 +0100

    Update generate_datelist.sql

--- bootcamp/materials/2-fact-data-modeling/lecture-lab/generate_datelist.sql
+++ bootcamp/materials/2-fact-data-modeling/lecture-lab/generate_datelist.sql
@@ -1,10 +1,9 @@
-
 WITH starter AS (
     SELECT uc.dates_active @> ARRAY [DATE(d.valid_date)]   AS is_active,
            EXTRACT(
                DAY FROM DATE('2023-03-31') - d.valid_date) AS days_since,
            uc.user_id
-    FROM users_cumulated uc
+    FROM users_cumulated uc -- one raw per user with list of active dates 
              CROSS JOIN
          (SELECT generate_series('2023-02-28', '2023-03-31', INTERVAL '1 day') AS valid_date) as d
     WHERE date = DATE('2023-03-31')
@@ -13,11 +12,31 @@ WITH starter AS (
          SELECT user_id,
                 SUM(CASE
                         WHEN is_active THEN POW(2, 32 - days_since)
-                        ELSE 0 END)::bigint::bit(32) AS datelist_int,
-                DATE('2023-03-31') as date
+                        ELSE 0 END)::bigint::bit(32) AS datelist_int
          FROM starter
          GROUP BY user_id
      )
 
-     INSERT INTO user_datelist_int
-     SELECT * FROM bits
+SELECT
+       user_id,
+       datelist_int,
+       BIT_COUNT(datelist_int) > 0 AS monthly_active,
+       BIT_COUNT(datelist_int) AS l32,
+       BIT_COUNT(datelist_int &
+       CAST('11111110000000000000000000000000' AS BIT(32))) > 0 AS weekly_active,
+       BIT_COUNT(datelist_int &
+       CAST('11111110000000000000000000000000' AS BIT(32)))  AS l7,
+
+       BIT_COUNT(datelist_int &
+       CAST('00000001111111000000000000000000' AS BIT(32))) > 0 AS weekly_active_previous_week
+      
+FROM bits;
+
+-- ADDED BY ME
+
+-- hystory: what days where active
+SELECT
+    user_id,
+    SUM(datelist_int)
+FROM bits
+GROUP BY user_id
